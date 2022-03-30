b1a6753cade497efb70e5f2eccd721e84d44c11b
Shvec_D <schwets.mitia-shvets@yandex.ru>
2022-03-30 21:19:08 +0300

    Revert "adding tasks oop1 and oop2"
    This reverts commit a67e7c9cddac5e0c282fe123536a837d8bf9c149.

--- MovingBallJavaFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.netcracker.movingballjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.netcracker.movingballjavafx to javafx.fxml;
-    exports com.netcracker.movingballjavafx;
-}
--- TasksOOP/src/com/netcracker/projects/tasks/taskoop/geomFigures/BaseFigure.java
+++
@@ -1,5 +0,0 @@
-package com.netcracker.projects.tasks.taskoop.geomFigures;
-
-public interface BaseFigure {
-    public double calculateArea();
-}
